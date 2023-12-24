b0afdde000ae4d3e06538e306b235a013d7e3789
RashadXalil <rashadmkh@code.edu.az>
2023-12-24 21:52:51 +0400

    tt

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const userRouter = require("./routes/User.routes")
 const app = express()
 app.use(express.json())
 app.use("/users", userRouter)
+app.get("/", (req, res) => {
+    res.send("hello world")
+})
 
 mongoose.connect("mongodb+srv://rashadkhll:rashad918@cluster0.vllij5v.mongodb.net/")
 const connection = mongoose.connection
